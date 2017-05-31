2300d59e89c3cf0e507a954c1fbd306b9d9683eb
Aram Joseph <ajoseph@utc4me.org>
2017-05-31 10:13:38 -0400

    Got goop

--- Assets/GIFPlayer.cs
+++ Assets/GIFPlayer.cs
@@ -9,13 +9,24 @@ public class GIFPlayer : MonoBehaviour
 
     [SerializeField] private int health = 12;
 
+    [SerializeField]
+    int equipmentCount = 2;
+    [SerializeField]
+    Transform currEquip;
+
     private Image[] heartBits;
     [SerializeField] private float damageBuffer;
     private float origDamageBuffer;
     private int heartIndex = 0;
     private bool damaged = false;
+    [SerializeField]
+    private Transform[] equipment = new Transform[2];
     void Start()
     {
+        for (int i = 0; i < equipmentCount; i++)
+        {
+            equipment[i] = transform.GetChild(0).GetChild(0).GetChild(i);
+        } 
         origDamageBuffer = damageBuffer;
         heartBits = GameObject.Find("HeartBits").GetComponentsInChildren<Image>();
     }
